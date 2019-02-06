2bd5ee9186ec8ec091c26fcd7b20d4de865f47af
Rom Ezer <rom@dotbcs.com>
2019-02-06 11:02:59 +0200

    Setup start script and port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const _ = require('lodash');
 
 var conver = require('./models/convert');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -14,7 +14,7 @@ app.post('/convert',(req,res) =>{
 	conver.run(req.body.filename, req.body.fileextension, req.body.documentState, req.body.contentversionid, req.body.url, req.body.token, req.body.objId, req.body.recordType, req.body.appSid, req.body.appKey).then((message) =>{
 		res.status(200).send(message);
 	}, (error) =>{
-		res.status(400).send('very bad request ' + JSON.stringify(error));
+		res.status(400).send('bad request ' + JSON.stringify(error));
 	});
 });
 
@@ -24,6 +24,6 @@ app.post('/convert',(req,res) =>{
 
 
 
-app.listen(3000, ()=> {
-	console.log('Started on port 3000');
+app.listen(port, ()=> {
+	console.log(`Started on port ${port}`);
 });
